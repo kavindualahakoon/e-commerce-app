e4472175556903decb0fa8fb70433f08a399b804
Kavindu Alahakoon <kavindu9805@gmail.com>
2024-07-20 19:37:22 +0530

    Fourth Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Body from "./Components/Body/Body";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+
+function App() {
+  return (
+    <div className="w-full h-screen">
+      <Header />
+
+      <Body />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { IconButton } from "@mui/material";
+
+const Header = () => {
+  return (
+    <header className=" fixed top-0 left-0 z-[100] w-full p-5 bg-my-background flex items-center justify-between drop-shadow-header-shadow">
+      {/* header left */}
+      <div className="flex items-center ">
+        <IconButton>
+          <MenuIcon className="text-red-600" />
+        </IconButton>
+        <h1 className=" ml-2 text-sm font-bold">
+          KV <span className=" text-red-600">SHOP</span>
+        </h1>
+        <div className=" overflow-hidden ml-2  flex items-center rounded-full bg-[#fcf7f7]">
+          <input
+            type="text"
+            placeholder="search"
+            className=" ml-1 outline-none p-2 font-semibold text-sm w-[200px] bg-inherit"
+          />
+          <SearchOutlinedIcon className="text-gray-400 mr-2" />
+        </div>
+      </div>
+      {/* header right */}
+      <ShoppingCartOutlinedIcon />
+    </header>
+  );
+};
+
+export default Header;
